6c29250c732c7ceda3de00b6046ca828f04b6e1d
Hepsko <Adrian778@op.pl>
2020-11-10 01:20:59 +0100

    properties configuration

---
+++ src/main/java/com/project/todolist/controller/InfoController.java
@@ -0,0 +1,31 @@
+package com.project.todolist.controller;
+
+
+import com.project.todolist.TaskConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    private TaskConfigurationProperties myProp;
+    private DataSourceProperties dataSource;
+
+    public InfoController(final TaskConfigurationProperties myProp, final DataSourceProperties dataSource) {
+        this.myProp = myProp;
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping("/info/url")
+    String url(){
+        return dataSource.getUrl();
+    }
+    @GetMapping("/info/prop")
+    boolean myProp()
+    {
+        return myProp.isAllowMultipleTasksFromTemplate();
+
+    }
+}
