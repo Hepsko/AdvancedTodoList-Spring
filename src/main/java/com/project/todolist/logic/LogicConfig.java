58a303749c77d619cf30b925a2eb47b8bd743c6f
Hepsko <Adrian778@op.pl>
2020-12-27 20:54:57 +0100

    class dependence on Spring

---
+++ src/main/java/com/project/todolist/logic/LogicConfig.java
@@ -0,0 +1,30 @@
+package com.project.todolist.logic;
+
+
+import com.project.todolist.TaskConfigurationProperties;
+import com.project.todolist.model.ProjectRepository;
+import com.project.todolist.model.TaskGroupRepository;
+import com.project.todolist.model.TaskRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogicConfig {
+    @Bean
+    ProjectService projectService(
+        final ProjectRepository repository,
+        final TaskGroupRepository taskGroupRepository,
+        final TaskConfigurationProperties  config
+    ){
+        return new ProjectService(repository,taskGroupRepository,config);
+    }
+
+    @Bean
+    TaksGroupService taksGroupService(
+            TaskGroupRepository repository,
+            TaskRepository taskRepository
+    )
+    {
+        return new TaksGroupService(repository,taskRepository);
+    }
+}
--- src/main/java/com/project/todolist/logic/TaksGroupService.java
+++ src/main/java/com/project/todolist/logic/TaksGroupService.java
@@ -7,14 +7,12 @@ import com.project.todolist.model.TaskGroupRepository;
 import com.project.todolist.model.TaskRepository;
 import com.project.todolist.model.projection.GroupReadModel;
 import com.project.todolist.model.projection.GroupWriteModel;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.RequestScope;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
-@RequestScope
+
 public class TaksGroupService {
    private TaskGroupRepository repsitory;
     private TaskRepository taskRepository;
