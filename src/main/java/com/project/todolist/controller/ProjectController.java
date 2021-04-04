d6358a3a4ae9e5422b0c3d504057f2494bb2645e
Hepsko <Adrian778@op.pl>
2021-04-04 15:46:58 +0200

    basic html

--- src/main/java/com/project/todolist/controller/ProjectController.java
+++ src/main/java/com/project/todolist/controller/ProjectController.java
@@ -1,14 +1,23 @@
 package com.project.todolist.controller;
 
+import com.project.todolist.model.ProjectStep;
+import com.project.todolist.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Component
 @RequestMapping("/projects")
 public class ProjectController {
     @GetMapping
-    String showProjects(){
+    String showProjects(Model model){
+        model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current ){
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
--- src/main/java/com/project/todolist/model/projection/ProjectWriteModel.java
+++ src/main/java/com/project/todolist/model/projection/ProjectWriteModel.java
@@ -5,6 +5,7 @@ import com.project.todolist.model.ProjectStep;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -12,7 +13,11 @@ public class ProjectWriteModel {
     @NotBlank(message = "Project's description must be not empty")
     private String description;
     @Valid
-    List<ProjectStep> steps;
+    List<ProjectStep> steps = new ArrayList<>();
+
+    public ProjectWriteModel(){
+        steps.add(new ProjectStep());
+    }
 
     public String getDescription() {
         return description;
